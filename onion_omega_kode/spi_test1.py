16de9c528373fe8b8e4a56ee179a666b40fa0c80
anoertoft <99796021+anoertoft@users.noreply.github.com>
2023-09-13 12:22:32 +0200

    added stuff

--- onion_omega_kode/spi_test1.py
+++ onion_omega_kode/spi_test1.py
@@ -9,12 +9,13 @@ spi = spidev.SpiDev()
 spi.open(spi_bus, spi_device)
 
 # Set SPI speed and mode
-spi.max_speed_hz = 20000 # 20 kHz
-spi.mode = 0 # Clock is low when idle, data is read on rising edge
+spi.max_speed_hz = 20000  # 20 kHz
+spi.mode = 0  # Clock is low when idle, data is read on rising edge
 
 # Read data from SPI bus
 try:
     while True:
+        print("Reading data from SPI bus")
         data = spi.xfer([0x00, 0x00, 0x00, 0x00])
         print(data)
 except KeyboardInterrupt:
